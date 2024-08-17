b6734d29c686f764fa8081cd46b339014ed0c7f7
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-08-18 00:02:58 +0600

    redux initiated

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import {
 import Home from './home/Home.jsx';
 import About from './about/About.jsx';
 import Contact from './contact/Contact.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 const router = createBrowserRouter([
   {
     path:"/",
@@ -27,6 +29,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
      <RouterProvider router={router} />
+     </Provider>
   </StrictMode>,
 )
