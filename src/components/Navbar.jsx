d75169bfd63e31791c3814f3cea9a5fd47b2f32e
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-08-18 20:05:20 +0600

    navbar initiated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { NavLink } from "react-router-dom"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="bg-emerald-300 mx-auto">
+      <div className="flex justify-around items-center ">
+        <NavLink>Home</NavLink>
+        <NavLink>Contact</NavLink>
+        <NavLink>About</NavLink>
+      </div>
+    </div>
   )
 }
 
