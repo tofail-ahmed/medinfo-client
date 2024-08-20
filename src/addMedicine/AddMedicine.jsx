98ff22444adbd3e9e143e481c822ff4a6ccda289
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-08-20 15:08:25 +0600

    addMedicine api and setup done

--- src/addMedicine/AddMedicine.jsx
+++ src/addMedicine/AddMedicine.jsx
@@ -1,46 +1,94 @@
 import { TextField, Button, Grid } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useCreateMedicineMutation } from "../redux/medicine/medicinesApi";
 
 const AddMedicine = () => {
+      const [createMedicine,{data,isLoading,error}]=useCreateMedicineMutation()
+
   const [medicineData, setMedicineData] = useState({
     medicine_name: "",
     generic_name: "",
     company_name: "",
-    alt_medicines: "",
+    alt_medicines: [],
     description: "",
     doses: "",
-    side_effects: "",
+    side_effects: [],
     actions: "",
-    interactions: "",
-    uses: "",
+    interactions: [],
+    uses: [],
     sold: 0,
-    warnings: "",
+    warnings: [],
   });
 
   const handleChange = (e) => {
     setMedicineData({ ...medicineData, [e.target.name]: e.target.value });
   };
+  // const handleChange = () => {
+  //       setMedicineData({ ...medicineData });
+  //     };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add your API call here to submit the form data
-    console.log(medicineData);
-  };
+    const altMedicinesArray = medicineData.alt_medicines
+      .split(",")
+      .map((item) => item.trim());
+    const sideEffectsArray = medicineData.side_effects
+      .split(",")
+      .map((item) => item.trim());
+    const interactionsArray = medicineData.interactions
+      .split(",")
+      .map((item) => item.trim());
+    const usesArray = medicineData.uses.split(",").map((item) => item.trim());
+    const warningsArray = medicineData.warnings
+      .split(",")
+      .map((item) => item.trim());
 
+    // Log the data including alt_medicines as an array
+//     console.log({
+//       ...medicineData,
+//       alt_medicines: altMedicinesArray,
+//       side_effects: sideEffectsArray,
+//       interactions: interactionsArray,
+//       uses: usesArray,
+//       warnings: warningsArray,
+//     });
+    const newMedicine={
+      ...medicineData,
+      alt_medicines: altMedicinesArray,
+      side_effects: sideEffectsArray,
+      interactions: interactionsArray,
+      uses: usesArray,
+      warnings: warningsArray,
+    }
+    createMedicine(newMedicine)
+    console.log(newMedicine)
+    
+  };
+  useEffect(() => {
+      if (isLoading) {
+        alert("Medicine adding in process, please wait...");
+      }
+      if (data && data.success) {
+        alert("Medicine added successfully");
+      }
+      if(error&&error.status===409){
+            alert(error.data.message)
+      }
+    }, [data, isLoading,error]);
+  
   return (
     <div>
       <h1>Add New Medicine</h1>
-      <form onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit}>
         {/* <Grid item xs={12} sm={4} md={6} > */}
-        <Grid container spacing={2} sx={{ my: 5}} className=" ">
+        <Grid container spacing={2} sx={{ my: 5 }} className=" ">
           <Grid item xs={12} sm={12} md={6}>
             <TextField
-            sx={{
-                  maxWidth: "80%",
-                  mx: "auto",
-                  display: "block"
-                  
-                }}
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
               label="Medicine Name"
               variant="outlined"
               size="small"
@@ -50,14 +98,31 @@ const AddMedicine = () => {
               onChange={handleChange}
             />
           </Grid>
+
           <Grid item xs={12} sm={12} md={6}>
             <TextField
-            sx={{
-                  maxWidth: "80%",
-                  mx: "auto",
-                  display: "block"
-                }}
-            fullWidth
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Company Name"
+              variant="outlined"
+              size="small"
+              name="company_name"
+              value={medicineData.company_name}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
               label="Generic Name"
               variant="outlined"
               size="small"
@@ -67,191 +132,187 @@ const AddMedicine = () => {
             />
           </Grid>
           <Grid item xs={12} sm={12} md={6}>
-          <TextField
-          sx={{
-            maxWidth: "80%",
-            mx: "auto",
-                  display: "block"
-          }}
-          fullWidth
-            label="Company Name"
-            variant="outlined"
-            size="small"
-            name="company_name"
-            value={medicineData.company_name}
-            onChange={handleChange}
-          />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-          <TextField
-          sx={{
-            maxWidth: "80%",
-            mx: "auto",
-                  display: "block"
-          }}
-          fullWidth
-          label="Alternative Medicines (comma-separated)"
-          variant="outlined"
-          size="small"
-          name="alt_medicines"
-          value={medicineData.alt_medicines}
-          onChange={handleChange}
-        />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-          <TextField
-          sx={{
-            maxWidth: "80%",
-            mx: "auto",
-                  display: "block"
-          }}
-            fullWidth
-            label="Description"
-            variant="outlined"
-            size="small"
-            name="description"
-            multiline
-            rows={4}
-            value={medicineData.description}
-            onChange={handleChange}
-          />
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-          <TextField
-           sx={{
-            maxWidth: "80%",
-            mx: "auto",
-                  display: "block"
-          }}
-            fullWidth
-            label="Doses"
-            variant="outlined"
-            size="small"
-            name="doses"
-            value={medicineData.doses}
-            onChange={handleChange}
-          />
-          </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-        <TextField
-            sx={{
-                  maxWidth: "80%",
-                  mx: "auto",
-                  display: "block"
-                }}
-            fullWidth
-            label="Side Effects (comma-separated)"
-            variant="outlined"
-            size="small"
-            name="side_effects"
-            value={medicineData.side_effects}
-            onChange={handleChange}
-          />
-        </Grid>
-         <Grid item xs={12} sm={12} md={6}>
-         <TextField
-           sx={{
-            maxWidth: "80%",
-            mx: "auto",
-                  display: "block"
-          }}
-            fullWidth
-            label="Actions"
-            variant="outlined"
-            size="small"
-            name="actions"
-            value={medicineData.actions}
-            onChange={handleChange}
-          />
-         </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-          <TextField
-            sx={{
-                  maxWidth: "80%",
-                  mx: "auto",
-                  display: "block"
-                }}
-            fullWidth
-            label="Interactions (comma-separated)"
-            variant="outlined"
-            size="small"
-            name="interactions"
-            value={medicineData.interactions}
-            onChange={handleChange}
-          />
-          </Grid>
-         <Grid item xs={12} sm={12} md={6}>
-         <TextField
-            sx={{
-                  maxWidth: "80%",
-                  mx: "auto",
-                  display: "block"
-                }}
-            fullWidth
-            label="Uses (comma-separated)"
-            variant="outlined"
-            size="small"
-            name="uses"
-            value={medicineData.uses}
-            onChange={handleChange}
-          />
-         </Grid>
-         <Grid item xs={12} sm={12} md={6}>
-         <TextField
-            sx={{
-              maxWidth: "80%",
-              mx: "auto",
-                  display: "block"
-            }}
-            fullWidth
-            label="Sold Count"
-            variant="outlined"
-            size="small"
-            name="sold"
-            type="number"
-            value={medicineData.sold}
-            onChange={handleChange}
-          />
-         </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-          <TextField
-            sx={{
-              maxWidth: "80%",
-              mx: "auto",
-                  display: "block"
-            }}
-            fullWidth
-            label="Warnings (comma-separated)"
-            variant="outlined"
-            size="small"
-            name="warnings"
-            value={medicineData.warnings}
-            onChange={handleChange}
-          />
-          </Grid>
-          
-           
-          
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-            <Button
-             sx={{
-                  maxWidth: {
-                    xs: "80%",  // 80% for extra small screens and up (mobile)
-                    lg: "90%",  // 90% for large screens and up (desktops)
-                  },
-                  mx: "auto",
-                  display: "block",
-                }}
-              type="submit"
-              variant="contained"
-              color="primary"
-              id="fullWidth"
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
               fullWidth
-            >
-              Add Medicine
-            </Button>
-            </Grid>
+              label="Sold Count"
+              variant="outlined"
+              size="small"
+              name="sold"
+              type="number"
+              value={medicineData.sold}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Alternative Medicines (comma-separated)"
+              variant="outlined"
+              size="small"
+              name="alt_medicines"
+              value={medicineData.alt_medicines}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Description"
+              variant="outlined"
+              size="small"
+              name="description"
+              multiline
+              rows={4}
+              value={medicineData.description}
+              onChange={handleChange}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Doses"
+              variant="outlined"
+              size="small"
+              name="doses"
+              value={medicineData.doses}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Side Effects (comma-separated)"
+              variant="outlined"
+              size="small"
+              name="side_effects"
+              value={medicineData.side_effects}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Actions"
+              variant="outlined"
+              size="small"
+              name="actions"
+              value={medicineData.actions}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Interactions (comma-separated)"
+              variant="outlined"
+              size="small"
+              name="interactions"
+              value={medicineData.interactions}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Uses (comma-separated)"
+              variant="outlined"
+              size="small"
+              name="uses"
+              value={medicineData.uses}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <TextField
+              sx={{
+                maxWidth: "80%",
+                mx: "auto",
+                display: "block",
+              }}
+              fullWidth
+              label="Warnings (comma-separated)"
+              variant="outlined"
+              size="small"
+              name="warnings"
+              value={medicineData.warnings}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <Button
+            sx={{
+              maxWidth: {
+                xs: "80%", // 80% for extra small screens and up (mobile)
+                lg: "90%", // 90% for large screens and up (desktops)
+              },
+              mx: "auto",
+              display: "block",
+            }}
+            type="submit"
+            variant="contained"
+            color="primary"
+            id="fullWidth"
+            fullWidth
+          >
+            Add Medicine
+          </Button>
+        </Grid>
       </form>
     </div>
   );
--- src/redux/medicine/medicinesApi.js
+++ src/redux/medicine/medicinesApi.js
@@ -1,31 +1,41 @@
 import { baseApi } from "../baseApi";
 
-const medicinesApi=baseApi.injectEndpoints({
-      endpoints:(builder)=>({
-            allMedicines:builder.query({
-                  query:()=>({
-                        url:"/medicines",
-                        method:"GET"
-                  }),
-                  providesTags:["medicines"]
-            }),
-            singleMedicine:builder.query({
-                  query:(id)=>({
-                        url:`/singleMedicine/${id}`,
-                        method:"GET"
-                  }),
-            
-            }),
-            searchMedicine: builder.query({
-                  query: (term) => ({
-                    url: `/medicines/search/${term}`,
-                    method: "GET",
-                  }),
-                  // providesTags: (result, error, term) => [{ type: 'Medicines', id: term }],
-                }),
+const medicinesApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    allMedicines: builder.query({
+      query: () => ({
+        url: "/medicines",
+        method: "GET",
       }),
-    
-      
-      })
+      providesTags: ["medicines"],
+    }),
+    singleMedicine: builder.query({
+      query: (id) => ({
+        url: `/singleMedicine/${id}`,
+        method: "GET",
+      }),
+    }),
+    searchMedicine: builder.query({
+      query: (term) => ({
+        url: `/medicines/search/${term}`,
+        method: "GET",
+      }),
+      // providesTags: (result, error, term) => [{ type: 'Medicines', id: term }],
+    }),
+    createMedicine: builder.mutation({
+      query: (body) => ({
+        url: "/medicines",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["medicines"],
+    }),
+  }),
+});
 
-export const {useAllMedicinesQuery,useSearchMedicineQuery,useSingleMedicineQuery}=medicinesApi
+export const {
+  useAllMedicinesQuery,
+  useSearchMedicineQuery,
+  useSingleMedicineQuery,
+  useCreateMedicineMutation,
+} = medicinesApi;
