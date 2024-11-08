6da8ac87bbdf79478cb0125a41e76f0caf4b6710
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-11-08 22:28:31 +0600

    review slide refactored

--- extra.js
+++ extra.js
@@ -19,3 +19,91 @@ function StyledText() {
 }
 
 export default StyledText;
+
+
+
+
+ {/* <div className="carousel__cell   number-slide1 ">
+              <div>
+                <img
+                  className="w-[50px] h-[50px] rounded-full mx-auto"
+                  src={allUser && allUser[0].imgUrl}
+                  alt=""
+                />
+                <span>{allUser && allUser[0].review}</span>
+                <div className="flex flex-col justify-end items-end">
+                  <span>--{allUser && allUser[0].name}</span>
+                  <span>--{allUser && allUser[0].email}</span>
+                </div>
+              </div>
+            </div>
+            <div className="carousel__cell   number-slide2 ">
+              <div>
+                <img
+                  className="w-[50px] h-[50px] rounded-full mx-auto"
+                  src={allUser && allUser[2].imgUrl}
+                  alt=""
+                />
+                <span>{allUser && allUser[2].review}</span>
+                <div className="flex flex-col justify-end items-end">
+                  <span>--{allUser && allUser[2].name}</span>
+                  <span>--{allUser && allUser[2].email}</span>
+                </div>
+              </div>
+            </div>
+            <div className="carousel__cell   number-slide3 ">
+              <div>
+                <img
+                  className="w-[50px] h-[50px] rounded-full mx-auto"
+                  src={allUser && allUser[2].imgUrl}
+                  alt=""
+                />
+                <span className="flex justify-center items-center flex-1">{allUser && allUser[2].review}</span>
+                <div className="flex flex-col justify-end items-end">
+                  <span>--{allUser && allUser[2].name}</span>
+                  <span>--{allUser && allUser[2].email}</span>
+                </div>
+              </div>
+            </div>
+            <div className="carousel__cell   number-slide4 ">
+              <div>
+                <img
+                  className="w-[50px] h-[50px] rounded-full mx-auto"
+                  src={allUser && allUser[3].imgUrl}
+                  alt=""
+                />
+                <span>{allUser && allUser[3].review}</span>
+                <div className="flex flex-col justify-end items-end">
+                  <span>--{allUser && allUser[3].name}</span>
+                  <span>--{allUser && allUser[3].email}</span>
+                </div>
+              </div>
+            </div>
+            <div className="carousel__cell   number-slide5 ">
+              <div>
+                <img
+                  className="w-[50px] h-[50px] rounded-full mx-auto"
+                  src={allUser && allUser[4].imgUrl}
+                  alt=""
+                />
+                <span>{allUser && allUser[4].review}</span>
+                <div className="flex flex-col justify-end items-end">
+                  <span>--{allUser && allUser[4].name}</span>
+                  <span>--{allUser && allUser[4].email}</span>
+                </div>
+              </div>
+            </div>
+            <div className="carousel__cell   number-slide6 ">
+              <div>
+                <img
+                  className="w-[50px] h-[50px] rounded-full mx-auto"
+                  src={allUser && allUser[5].imgUrl}
+                  alt=""
+                />
+                <span>{allUser && allUser[5].review}</span>
+                <div className="flex flex-col justify-end items-end">
+                  <span>--{allUser && allUser[5].name}</span>
+                  <span>--{allUser && allUser[5].email}</span>
+                </div>
+              </div>
+            </div> */}
--- src/Reviews/Reviews.jsx
+++ src/Reviews/Reviews.jsx
@@ -1,80 +1,74 @@
-import { Box, Grid, Typography, Paper, useMediaQuery } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useAllUserQuery } from "../redux/user/usersApi";
 import Loader from "../ComponentsTemp/Loader";
 import 'keen-slider/keen-slider.min.css';
-import KeenSlider from 'keen-slider';
-import { useKeenSlider } from "keen-slider/react"
-import "../Reviews/Review.css"
-
-
+import { useKeenSlider } from "keen-slider/react";
+import "../Reviews/Review.css";
 
 const carousel = (slider) => {
-      const z = 300
-      function rotate() {
-        const deg = 360 * slider.track.details.progress
-        slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`
-      }
-      slider.on("created", () => {
-        const deg = 360 / slider.slides.length
-        slider.slides.forEach((element, idx) => {
-          element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`
-        })
-        rotate()
-      })
-      slider.on("detailsChanged", rotate)
-    }
-
-    
+  const z = 360;
+  function rotate() {
+    const deg = 360 * slider.track.details.progress;
+    slider.container.style.transform = `translateZ(-${z}px) rotateY(${-deg}deg)`;
+  }
+  slider.on("created", () => {
+    const deg = 360 / slider.slides.length;
+    slider.slides.forEach((element, idx) => {
+      element.style.transform = `rotateY(${deg * idx}deg) translateZ(${z}px)`;
+    });
+    rotate();
+  });
+  slider.on("detailsChanged", rotate);
+};
 
 const Reviews = () => {
-      const [sliderRef] = useKeenSlider(
-            {
-              loop: true,
-              selector: ".carousel__cell",
-              renderMode: "custom",
-              mode: "free-snap",
-            },
-            [carousel]
-          )
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+      selector: ".carousel__cell",
+      renderMode: "custom",
+      mode: "free-snap",
+    },
+    [carousel]
+  );
 
-      const [heightSet, setHeightSrt] = useState(); // Initial count for large screens
-      const [fontSize, setFontSize] = useState(); // Initial count for large screens
-      const [imgWidth,setImgWidth ] = useState(); // Initial count for large screens
-      const [imgHeight,setImgHeight ] = useState(); // Initial count for large screens
-
-
+  const [heightSet, setHeightSrt] = useState();
+  const [fontSize, setFontSize] = useState();
+  const [imgWidth, setImgWidth] = useState();
+  const [imgHeight, setImgHeight] = useState();
 
   const { data, isLoading } = useAllUserQuery();
   const allUser = data?.data;
-  console.log(allUser&&allUser[0])
 
   const isLargeScreen = useMediaQuery("(min-width:1200px)");
-  const isMediumScreen = useMediaQuery(
-    "(min-width:900px) and (max-width:1199px)"
-  );
-  const isSmallScreen = useMediaQuery(
-    "(min-width:600px) and (max-width:899px)"
-  );
+  const isMediumScreen = useMediaQuery("(min-width:900px) and (max-width:1199px)");
 
   useEffect(() => {
-       
-        if (isLargeScreen) setHeightSrt("400px"), setImgWidth("100px"),setImgHeight("100px"),setFontSize("16px");
-        else if (isMediumScreen) setHeightSrt("400px"),setImgWidth("80px"),setImgHeight("80px"),setFontSize("14px");
-        // else if (isSmallScreen) setDisplayCount(10);
-        else setHeightSrt("300px"),setImgWidth("50px"),setImgHeight("50px"),setFontSize("12px"); // Default for extra-small screens
-      
-    }, [isLargeScreen, isMediumScreen, isSmallScreen]);
+    if (isLargeScreen) {
+      setHeightSrt("600px");
+      setImgWidth("100px");
+      setImgHeight("100px");
+      setFontSize("16px");
+    } else if (isMediumScreen) {
+      setHeightSrt("400px");
+      setImgWidth("80px");
+      setImgHeight("80px");
+      setFontSize("14px");
+    } else {
+      setHeightSrt("300px");
+      setImgWidth("50px");
+      setImgHeight("50px");
+      setFontSize("12px");
+    }
+  }, [isLargeScreen, isMediumScreen]);
 
   if (isLoading) {
     return <Loader />;
   }
 
-
-
-
   return (
-    <div>
+    <div className="my-20">
       <Typography
         sx={{
           fontSize: "24px",
@@ -90,29 +84,43 @@ const Reviews = () => {
         All Reviews
       </Typography>
 
-     
       <div className="wrapper my-40">
-      <div className="scene">
-        <div className="carousel keen-slider" ref={sliderRef}>
-          <div className="carousel__cell   number-slide1 ">
-            
-            <div>
-            <img className="w-[50px] h-[50px] rounded-full mx-auto" src={allUser&&allUser[0].imgUrl} alt="" />
-            <span>{allUser&&allUser[0].review}</span>
-           <div className="flex flex-col justify-end items-end">
-           <span>--{allUser&&allUser[0].name}</span>
-           <span>--{allUser&&allUser[0].email}</span>
+        <div className="scene">
+          <div className="carousel keen-slider heightSet" ref={sliderRef} >
+            {allUser?.slice(0, 6).map((user, index) => (
+           <div key={index} className={`carousel__cell number-slide${index + 1} heightSet`}>
+           <div className="flex flex-col items-center h-full justify-between">
+             <img
+               className="rounded-full"
+               src={user.imgUrl}
+               alt=""
+               style={{ width: imgWidth, height: imgHeight }}
+             />
+             
+             {/* Review text takes up remaining space, pushing email and name to bottom */}
+             <span
+               style={{ fontSize: fontSize }}
+               className="text-justify mt-2 flex-grow"
+             >
+               {user.review}
+             </span>
+             
+             {/* Email and Name section aligned at the bottom */}
+             <div
+               className="mt-2 flex flex-col absolute right-2 bottom-2 fontSize"
+               style={{ fontSize: fontSize }}
+             >
+               <span className="text-[12px]"> {user.name}</span>
+               <span className="text-[10px]"> {user.email}</span>
+             </div>
            </div>
-            </div>
+         </div>
+         
+          
+            ))}
           </div>
-          <div className="carousel__cell lg:w-[350px]  w-[300px] number-slide2">{allUser&&allUser[1].review}</div>
-          <div className="carousel__cell lg:w-[350px]  w-[300px] number-slide3">{allUser&&allUser[2].review}</div>
-          <div className="carousel__cell lg:w-[350px]  w-[300px] number-slide4">{allUser&&allUser[3].review}</div>
-          <div className="carousel__cell lg:w-[350px]  w-[300px] number-slide5">{allUser&&allUser[4].review}</div>
-          <div className="carousel__cell lg:w-[350px]  w-[300px] number-slide6">{allUser&&allUser[5].review}</div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
