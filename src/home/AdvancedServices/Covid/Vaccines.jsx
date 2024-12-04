3b164515223f602a35dbe1666879aa7b64b42a96
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-12-04 20:50:13 +0600

    vaccine page initiated

--- src/home/AdvancedServices/Covid/Vaccines.jsx
+++ src/home/AdvancedServices/Covid/Vaccines.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Vaccines = () => {
   return (
-    <div className='my-24'>
-      <h1 className='font-extrabold text-md lg:text-3xl text-center'>Select upto 4 Vaccines</h1>
+    <div className='my-24 max-w-[70%] lg:max-w-[50%] mx-auto'>
+      <div className='flex flex-col justify-center text-center items-start mx-auto'>
+      <h1 className='font-extrabold text-md lg:text-3xl '>Select upto 4 Vaccines</h1>
      
             
-            <h1 className='text-center flex justify-start mx-auto items-center'>Below are the vaccinations the patient can schedule based on their age and location.</h1>
+     <h1 className=''>Below are the vaccinations the patient can schedule based on their age and location.</h1>
 
-<h1 className="text-start">CDC says it’s safe to get multiple vaccinations at once.</h1>
+<h1 className="">CDC says it’s safe to get multiple vaccinations at once.</h1>
+      </div>
 
     </div>
   )
