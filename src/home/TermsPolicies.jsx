9f56cefda5bf6b272b17a3b907de80e7078b9b5d
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-01-03 21:23:05 +0600

    terms page wrote little

--- src/home/TermsPolicies.jsx
+++ src/home/TermsPolicies.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TermsPolicies = () => {
   return (
-    <div className="lg:mx-[20%] mx-[5%] my-14">
+    <div className="lg:mx-[20%] mx-[5%] my-14 text-justify">
       <h1 className="mt-16  lg:text-6xl text-2xl text-green-300 mb-10 font-extrabold text-center py-10 uppercase">
         Terms of Service
       </h1>
@@ -11,8 +11,6 @@ const TermsPolicies = () => {
         ACTION WAIVER, WHICH AFFECT YOUR LEGAL RIGHTS. YOU SHOULD REVIEW THIS
         DOCUMENT IN ITS ENTIRETY BEFORE ACCESSING OR USING THE ONLINE SERVICES.
       </h1>
-
-
 
       <h1 className="my-6 lg:text-3xl text-xl text-blue-950 font-semibold">
         Disclosures
@@ -52,15 +50,129 @@ const TermsPolicies = () => {
         and (ii) either a printer or storage space on such device.
       </h1>
 
-
       <h1 className="my-6 lg:text-3xl text-xl text-blue-950 font-semibold">
-      Important Note Regarding Content of Online Services
-
+        Important Note Regarding Content of Online Services
       </h1>
       <h1 className="my-6 text-lg">
-      The information and content (collectively, “Content”) on the Online Services is for your general educational information only and does not constitute professional medical advice, diagnosis, treatment, or recommendations of any kind by the Company. You should call your health care provider if you have questions, at any time, about your health. All information provided by the Company, or in connection with any communications supported by the Company, is intended to be for general information purposes only, and is in no way intended to create a provider/patient relationship as defined by state or federal law.
-<br />
-You are responsible for protecting your username (e.g., the email address and password that you provide when registering for Online Services) or other activation codes, and if they are compromised, you agree to change your username and password and immediately contact us.
+        The information and content (collectively, “Content”) on the Online
+        Services is for your general educational information only and does not
+        constitute professional medical advice, diagnosis, treatment, or
+        recommendations of any kind by the Company. You should call your health
+        care provider if you have questions, at any time, about your health. All
+        information provided by the Company, or in connection with any
+        communications supported by the Company, is intended to be for general
+        information purposes only, and is in no way intended to create a
+        provider/patient relationship as defined by state or federal law.
+        <br />
+        You are responsible for protecting your username (e.g., the email
+        address and password that you provide when registering for Online
+        Services) or other activation codes, and if they are compromised, you
+        agree to change your username and password and immediately contact us.
+      </h1>
+
+      <h1 className="my-6 lg:text-3xl text-xl text-blue-950 font-semibold">
+        Restrictions on Use of Online Services
+      </h1>
+      <h1 className="my-4">You will not-</h1>
+      <ol style={{ listStyleType: "square", paddingLeft: "20px" }}>
+        <li>
+          use our Online Services or Content in any way not expressly permitted
+          by these Terms;
+        </li>
+        <li>
+          copy, modify or harvest data, Content, or materials from the Online
+          Services;
+        </li>
+        <li>
+          remove or alter, any copyright or other proprietary rights or notices
+          on the Online Services;
+        </li>
+        <li>misrepresent your identity or provide any false information;</li>
+        <li>interfere with the operation of our Online Services;</li>
+        <li>
+          share any password with any third parties or use any third‐party’s
+          password;
+        </li>
+        <li>
+          engage in commercial, competitive, or viral messaging, or sending of
+          unsolicited advertisements, or similar communications, including
+          harmful computer code, viruses, or malware;
+        </li>
+        <li>
+          use the Online Services in any way that could, in our sole judgment,
+          interfere with any other party's use or enjoyment of the Online
+          Services, impair our networks or servers, or expose us or any third
+          party to any claims or liability whatsoever, or use software or other
+          means to access, "scrape," "crawl," or "spider" any web pages or other
+          services from the Online Services. If you are blocked from the Online
+          Services (including by blocking your IP address), you agree not to
+          implement any measures to circumvent such blocking;
+        </li>
+        <li>
+          directly or indirectly authorize anyone else to take actions
+          prohibited in this section; or
+        </li>
+        <li>
+          attempt to reverse engineer any of the software used to provide the
+          Online Services.
+        </li>
+      </ol>
+      <h1 className="my-4">You agree that-</h1>
+      <ol style={{ listStyleType: "square", paddingLeft: "20px" }}>
+        <li>you will comply with all applicable laws and regulations; and</li>
+        <li>
+          you represent and warrant that you are at least 13 years of age, and
+          that, if you are between 13 and the age of majority in your state and
+          otherwise not emancipated, a parent and/or guardian agrees to these
+          Terms on your behalf.
+        </li>
+      </ol>
+      <h1 className="my-6">
+        We may take any measures we deem appropriate, in our sole discretion and
+        permitted by law, to enforce these Terms.
+      </h1>
+
+      <h1 className="my-6 lg:text-3xl text-xl text-blue-950 font-semibold">
+        Posting Messages, Comments or Content
+      </h1>
+      <h1 className="my-4">You will not-</h1>
+      <ol style={{ listStyleType: "square", paddingLeft: "20px" }}>
+        <li>
+          post any improper or inappropriate content, including that which is
+          obscene, libelous, harassing, threatening, harmful, inflammatory,
+          invasive of privacy, or otherwise objectionable, constitutes
+          advertising or solicitation, or violates applicable law.
+        </li>
+      </ol>
+      <h1 className="my-4">You Agree That-</h1>
+      <ol style={{ listStyleType: "square", paddingLeft: "20px" }}>
+        <li>
+          any content posted to the Online Services by third parties is only the
+          opinion of the poster, is no substitute for your own research, and
+          should not be relied upon for any purpose.
+        </li>
+      </ol>
+      <h1 className="my-6">
+        You are solely responsible for the content of any postings that you
+        submit. Any content you submit will be routed through the Internet and
+        you understand and acknowledge that you have no expectation of privacy
+        regarding that content. Never assume that you are anonymous and cannot
+        be identified by your posts.
+      </h1>
+      <h1 className="my-6">
+        If you do post content or submit material to the Online Services, and
+        unless we indicate otherwise, you grant the Company a non-exclusive,
+        royalty-free, perpetual, irrevocable, and fully sublicensable right to
+        use, reproduce, modify, adapt, publish, translate, create derivative
+        works from, distribute, and display such content throughout the world in
+        any media. You grant the Company and any of its sublicensees the right
+        to use the name that you submit in connection with such content, if they
+        choose. You represent and warrant that you own or otherwise control all
+        of the rights to the content that you post; that the content is
+        accurate; that use of the content you supply does not violate these
+        Terms and will not cause injury to any person or entity; and that you
+        will indemnify the Company or its affiliates for all claims resulting
+        from any content that you supply.
       </h1>
     </div>
   );
