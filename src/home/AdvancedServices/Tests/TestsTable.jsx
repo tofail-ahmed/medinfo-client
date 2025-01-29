515c0f417f7bfe7a21b4d29028cf62b96920d9fd
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-01-29 22:04:09 +0600

    ...

--- src/home/AdvancedServices/Tests/TestsTable.jsx
+++ src/home/AdvancedServices/Tests/TestsTable.jsx
@@ -28,16 +28,16 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(Test_type, Cost, Result_time, Age) {
+  return { Test_type, Cost, Result_time, Age };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 159, 6.0, 24),
+  createData('Ice cream sandwich', 237, 9.0, 37),
+  createData('Eclair', 262, 16.0, 24),
+  createData('Cupcake', 305, 3.7, 67),
+  createData('Gingerbread', 356, 16.0, 49),
 ];
 
 export default function TestsTable() {
@@ -46,23 +46,23 @@ export default function TestsTable() {
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            <StyledTableCell>Test type</StyledTableCell>
+            <StyledTableCell align="right">Cost</StyledTableCell>
+            <StyledTableCell align="right">Result time</StyledTableCell>
+            <StyledTableCell align="right">Age</StyledTableCell>
+            {/* <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <StyledTableRow key={row.name}>
+            <StyledTableRow key={row.Test_type}>
               <StyledTableCell component="th" scope="row">
-                {row.name}
+                {row.Test_type}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+              <StyledTableCell align="right">{row.Cost}</StyledTableCell>
+              <StyledTableCell align="right">{row.Result_time}</StyledTableCell>
+              <StyledTableCell align="right">{row.Age}</StyledTableCell>
+              {/* <StyledTableCell align="right">{row.protein}</StyledTableCell> */}
             </StyledTableRow>
           ))}
         </TableBody>
