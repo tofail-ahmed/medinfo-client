16bf550733b44f72d1cfc9152d32661e3cc653a4
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-03-23 21:13:00 +0600

    ...

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,12 +4,15 @@ import { useSelector } from "react-redux";
 
 import Container from "../ComponentsTemp/Container";
 import Footer from "../ComponentsTemp/Footer/Footer";
+import { useAllMedicinesQuery } from "../redux/medicine/medicinesApi";
 
 // import Banner from '../home/Banner';
 // import SearchResult from '../search/SearchResult';
 // import SearchResult from '../search/SearchResult';
 // import { useSearchMedicineQuery } from '../redux/medicine/medicinesApi';
 // import { useState } from 'react';
+
+
 
 const MainLayout = () => {
   const darkMode = useSelector((store) => store.theme.darkMode);
@@ -20,6 +23,8 @@ const MainLayout = () => {
   //   skip: !searchTerm,
   // });
 
+
+  
   return (
     <div className={`${darkMode ? "bg-slate-700 text-white" : ""}`}>
       <Navbar />
