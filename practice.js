ebb376ef7aef412bbda6fab9e55ca15a06140a60
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-03-25 19:35:55 +0600

    practice.js added

---
+++ practice.js
@@ -0,0 +1,19 @@
+// * 1
+const cameCase=()=>{
+      const favFood="biriyani"
+      const favDrink="borhani"
+      console.log(favFood+"+"+favDrink)
+}
+
+cameCase()
+
+
+// * 2
+const camelcase=()=>{
+      const favFood="Biriyani"
+      const favDrink="Borhani"
+      console.log(favFood+"+"+favDrink)
+}
+
+camelcase()
+
