443e43b505501645324eab030ebe4eec23d70465
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-03-31 16:28:21 +0600

    ...

--- practice.js
+++ practice.js
@@ -5,7 +5,7 @@ const cameCase=()=>{
       console.log(favFood+"+"+favDrink)
 }
 
-cameCase()
+// cameCase()
 
 
 // * 2
@@ -15,5 +15,28 @@ const camelcase=()=>{
       console.log(favFood+"+"+favDrink)
 }
 
-camelcase()
+// camelcase()
 
+const city="ctg"
+const City="dhaka"
+// console.log(city,City)
+
+const fatherAge="55"
+const sonAge="22"
+const fatherAgeNumber=parseInt(fatherAge)
+const sonAgeNumber=parseInt(sonAge)
+
+// console.log(typeof(fatherAgeNumber))
+// console.log(typeof(sonAgeNumber))
+// console.log(fatherAgeNumber+sonAgeNumber)
+
+
+
+// console.log(20==="20")
+// console.log("true"==true)
+ for(let i=0,j=0;i<=10 && j<=10;){
+      
+      // console.log("i--->",++i);
+      // console.log("j--->",++j);
+ }
+ console.log(10!=10)
