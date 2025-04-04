606aeeef7e605590fb89ea78810c97eafbf6f8b4
Tofail Ahmed Sayem <atofail50@gmail.com>
2025-04-04 23:42:35 +0600

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -19,12 +9,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  // Enforce case sensitivity in file paths
-  fs: {
-    strict: true, // treat file paths as case-sensitive
-  },
+  // server: {
+  //   historyApiFallback: true, // Enable fallback routing
+  // },
   build: {
-    chunkSizeWarningLimit: 1000, // in kBs
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
